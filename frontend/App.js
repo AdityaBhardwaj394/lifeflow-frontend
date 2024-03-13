a4303164bbf6cde143573ca45bb68603fcaf02c1
akshat209141 <12115077@nitkkr.ac.in>
2024-03-13 11:12:16 +0530

    adding api(progress)

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screen/Home'
 import DashBoard from './screen/DashBoard'
 import Login from './screen/Login'
 import Register from './screen/Register'
 import Splash from './screen/Splash'
+import Profile from './screen/Profile';
+import Donor from './screen/Donor';
+import RequestBlood from './screen/RequestBlood';
 import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import TabNavigator from './screen/TabNavigator';
 
 const Stack=createStackNavigator();
 
@@ -14,11 +20,15 @@ const App = () => {
   return (
     <NavigationContainer>
     <Stack.Navigator
-    initialRouteName="Splash">
+    initialRouteName="TabNavigator"
+    screenOptions={{ headerShown: false }} >
       <Stack.Screen name="Splash" component={Splash}/>
       <Stack.Screen name="DashBoard" component={DashBoard}/>
       <Stack.Screen name="login" component={Login}/>
       <Stack.Screen name="register" component={Register}/>
+      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Profile" component={Profile}/>
     </Stack.Navigator>
     </NavigationContainer>
   )
--- frontend/screen/Home.js
+++ frontend/screen/Home.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image,Button } from 'react-native'
 import React from 'react'
-
-const Home = () => {
+import Header from './Header';
+const Home = ({navigation}) => {
   return (
     <View>
-      <Text>Home</Text>
+        <Header navigation ={navigation}/>
+
+       <View style={{ flexDirection: 'row', justifyContent: 'space-around', padding: 10 }}>
+          <Button title="Schedule Donation" onPress={() => navigation.navigate('ScheduleDonation')} />
+         <Button title="Track Upcoming Donation" disabled={true} /> 
+       </View>
+
+       <View style={{ padding: 10 }}>
+         <Text>Blood Type: O+</Text>
+         <Text>Last Donation: 2024-02-14</Text>
+      </View>
+
     </View>
+ 
   )
-}
+};
 
 export default Home;
