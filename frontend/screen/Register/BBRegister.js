097f9658afc7c452ff5c70bf0548f6bd73e41978
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2024-03-14 17:16:24 +0530

    added email

--- frontend/screen/Register/BBRegister.js
+++ frontend/screen/Register/BBRegister.js
@@ -1,67 +1,68 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import Button from '../../components/Button';
-import TextInput from '../../components/TextInput';
-import Background from '../../components/Background';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { setBBNameRedux, setBBPhoneno, setBBemailRedux, setBBreg_no } from '../../store/hospitalSlice';
 import axios from 'axios';
-const BBRegister = ({route,navigation}) => {
-  const {hospital_ph,hospital_name,hospital_id,hospital_lat,hospital_lon}=route.params;
-  const [email, setEmail] = useState('')
-  const [password, setpassword] = useState('')
-  const [phone_number, setPhone] = useState('')
-  const [reg_no, setRegno] = useState('')
-  const dispatch=useDispatch()
-  // const [hospital_name, setHospital] = useState('')
-  
+import Background from '../../components/Background';
+import Button from '../../components/Button';
+import TextInput from '../../components/TextInput';
+import auth from '@react-native-firebase/auth'
+const BBRegister = ({ route, navigation }) => {
+  const { hospital_name,hospital_id} = route.params;
+  // console.log(route.params)
+  // console.log(hospital_id, hospital_name);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone_number, setPhone] = useState('');
+  const [reg_no, setRegno] = useState('');
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
-  const register= async ()=>{
-    await auth().createUserWithEmailAndPassword(email,password).then(
-      ()=>{
-        dispatch(setBBNameRedux(hospital_name));
-        dispatch(setBBPhoneno(phone_number));
-        dispatch(setBBemailRedux(email));
-        dispatch(setBBreg_no(reg_no));
-        axios.post('http://192.168.1.58:8001/entity/create', 
-        {
-          "primary_email": email,
-          "primary_ph_no": phone_number,
-          "reg_number": reg_no,
-          "name": hospital_name,
-        });
-         navigation.navigate('login')
-      }
-    ).catch(err=>{
+
+  const register = async () => {
+    try {
+      // Create user with email and password
+      await auth().createUserWithEmailAndPassword(email, password);
+      
+      // Dispatch Redux actions
+      dispatch(setBBNameRedux(hospital_name));
+      dispatch(setBBPhoneno(phone_number));
+      dispatch(setBBemailRedux(email));
+      dispatch(setBBreg_no(reg_no));
+      
+      // Post data to the backend
+      axios.post(`http://192.168.1.136:8001/entity/create?tomtom_id=${hospital_id}`, {
+        primary_email: email,
+        primary_ph_no: phone_number,
+        reg_number: reg_no,
+        name: hospital_name,
+      });
+
+      // Navigate to login screen
+      navigation.navigate('login');
+    } catch (err) {
       if (err.code === 'auth/email-already-in-use') {
         console.log('That email address is already in use!');
-      }
-  
-      if (err.code === 'auth/invalid-email') {
+      } else if (err.code === 'auth/invalid-email') {
         console.log('That email address is invalid!');
       }
-  
+
       console.error(err);
-    })
-    
-  }
+    }
+  };
+
   return (
-    
-       <Background>
-      {/* <TextInput placeholder="Email"value={email} onChangeText={setEmail} /> */}
+    <Background>
       <TextInput
-        label="email"
+        label="Email"
         returnKeyType="done"
         value={email}
         onChangeText={setEmail}
-        // secureTextEntry
       />
-      {/* <TextInput placeholder="password" value={password} onChangeText={setpassword}  /> */}
       <TextInput
         label="Password"
         returnKeyType="done"
         value={password}
-        onChangeText={setpassword}
+        onChangeText={setPassword}
         secureTextEntry
       />
       <TextInput
@@ -69,29 +70,24 @@ const BBRegister = ({route,navigation}) => {
         returnKeyType="done"
         value={phone_number}
         onChangeText={setPhone}
-        // secureTextEntry
       />
       <TextInput
         label="Hospital Name"
         returnKeyType="done"
         value={hospital_name}
-        // onChangeText={setHospital}
-        // secureTextEntry
+        editable={false} // Prevent editing
       />
       <TextInput
         label="Registration Number"
         returnKeyType="done"
         value={reg_no}
         onChangeText={setRegno}
-        // secureTextEntry
       />
       <Button loading={loading} mode="contained" onPress={register}>
         Register
       </Button>
-    
     </Background>
-    
-  )
-}
+  );
+};
 
-export default BBRegister
+export default BBRegister;
--- frontend/screen/Register/BBRegisterSearch.js
+++ frontend/screen/Register/BBRegisterSearch.js
@@ -2,29 +2,34 @@ import axios from 'axios';
 import React from 'react';
 import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useSelector } from "react-redux";
-// import { IP } from '../../config';
-const BBRegisterSearch = ({navigation}) => {
+
+const BBRegisterSearch = ({ navigation }) => {
   const [value, onChangeText] = React.useState('');
   const [res, setRes] = React.useState([]);
-  const lat = useSelector((state) => state.user.location.latitude)
-  const lon = useSelector((state) => state.user.location.longitude)
+  const lat = useSelector((state) => state.user.location.latitude);
+  const lon = useSelector((state) => state.user.location.longitude);
   
   const handleChange = text => {
     onChangeText(text);
-    console.log(lat,lon)
-    
-    axios
-      .get(
-        `http://192.168.1.58:8001/search?lat=${lat.toString()}&lon=${lon.toString()}&radius=10000&q=${text}`,
-        // http://${IP}:8001/search?lat=17.5054036&lon=78.4937645&radius=2000&q=ho
-      )
-      .then(result => {
-        console.log(result.data);
-        setRes(result.data);
-      });
+    console.log(lat, lon);
+    console.log(text);
+    try {
+      axios
+        .get(`http://192.168.1.136:8001/search?lat=${lat.toString()}&lon=${lon.toString()}&radius=10000&q=${text}`)
+        .then(result => {
+          // console.log(result.data);
+          setRes(result.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
   };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Search For Hospitals</Text>
       <View
         style={{
@@ -36,41 +41,35 @@ const BBRegisterSearch = ({navigation}) => {
           editable
           onChangeText={text => handleChange(text)}
           value={value}
-          style={{padding: 10}}
+          style={{ padding: 10 }}
         />
       </View>
       
       <FlatList
         data={res}
-        renderItem={({item}) => 
-        <Text style={styles.item} onPress={
-          () => navigation.navigate('BBRegister',{
+        keyExtractor={(item, index) => index.toString()} // Add a keyExtractor
+        renderItem={({ item }) => 
+          <Text style={styles.item} onPress={() => navigation.navigate('BBRegister', {
             hospital_name: item.poi.name,
-            hospital_id: item.poi.id,
-            hospital_lat: item.poi.lat,
-            hospital_lon: item.poi.lon,
-            hospital_ph:item.poi.phone
-          
-          })
-        }>{item.poi.name}</Text>
-    }
+            hospital_id: item.id,
+            
+          })}>{item.poi.name}</Text>
+        }
       />
-  
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 22,
-    },
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
-    },
-  })
-  
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
 export default BBRegisterSearch;
