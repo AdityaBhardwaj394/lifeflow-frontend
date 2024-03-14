aad79caa4289d024a454cc53f5f9c509c0dff4ab
akshat209141 <12115077@nitkkr.ac.in>
2024-03-14 18:03:42 +0530

    BB

--- frontend/screen/BloodBank/DonorBB.js
+++ frontend/screen/BloodBank/DonorBB.js
@@ -1,11 +1,36 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
 const DonorBB = () => {
+  const [id,setId] = useState(null);
+  const [emailBB,setEmailBB] = useSelector
+  const [res,setRes]= useState(null);
+  const [err,setErr] = useState(null);
+  useEffect(()=>{
+    const getData= async()=>{
+      try{
+        const response = await api.get(`/donor/${id}`);
+      }
+      catch(err)
+      {
+        console.log("Error:",err);
+        setErr(err);
+      }
+      finally{
+        setErr(null);
+      }
+    }
+    
+  },[])
   return (
-    <View>
-      <Text>DonorBB</Text>
-    </View>
+    <FlatList
+        data={res}
+        renderItem={({item}) => 
+
+        <View>
+          <Text>{item.poi.name}</Text>
+        </View>
+
+        }/>
   )
 }
 
--- frontend/screen/BloodBank/RecieverBB.js
+++ frontend/screen/BloodBank/RecieverBB.js
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native'
+import { useState } from 'react';
 import React from 'react'
 import api from '../api'
+import { useSelector } from 'react-redux';
 
 const RecieverBB = () => {
-  
+  const [id,setId] = useState(null);
+  const [emailBB,setEmailBB] = useSelector
+  useEffect(()=>{
+    const getData=()=>{
+      try{
+        const response = await api.get(`/waitlist/${id}`);
+      }
+    }
+  },[])
   return (
+
+    <FlatList
+        data={res}
+        renderItem={({item}) => 
+
+        <View>
+          <Text>{item.poi.name}</Text>
+        </View>
+
+        }/>
     
   )
 }
